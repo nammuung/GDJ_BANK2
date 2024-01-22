f70a3fe0e52d970b9b04c4c2f20346d6941b4719
nammuung <audrbs2368@naver.com>
2024-01-22 22:44:20 +0900

    add

--- src/main/webapp/resources/js/agreement.js
+++ src/main/webapp/resources/js/agreement.js
@@ -1,41 +1,43 @@
 console.log("agreement");
 
-//javascript
-// const checkAll = document.getElementById("chekcAll");
-// let a = document.getElementsByClassName(checks);
-// const checks = document.getElementsByClassName("checks");
+//js
+const checkAll = document.getElementById("checkAll");
+const checks = document.getElementsByClassName("checks");
 
-// checkAll.addEventListener("click",()=>{
-//     let v=checkAll.getAttribute("checked");
-//     console.log(v);
-//     v = checkAll.checked
-//     var len = a.length;
-//         for(var i=0; i<len; i++){
-//           x[i].checks = e;
-//         }
+checkAll.addEventListener("click", ()=>{
+    let v = checkAll.getAttribute("checked");
+    console.log("CHECK : ", v);
+    v = checkAll.checked;
+    
+    for(let check of checks){
+        check.checked=v;
+
+    }
+
+})
+let checkcount = 0;
+for(let c of checks){
+    c.addEventListener("click", function(){
+        console.log(this.checked);
+        if(this.checked == false){
+            checkAll.checked = false;
+        }else{
+            checkAll.checked = true;
+        }
+    })
+
+
+}
+
+
+
+//jquery
+// $('#checkAll').click(()=>{
+//     let v = $("#checkAll").prop("checked");
+    
+//     $('.checks').prop("checked", v);
 // });
 
-
-
- let a = document.getElementsByClassName(checks);
-// // jquery
-$('#checkAll').click(() => {
-    let v = $("#checkAll").prop("checked");
-    $('.checks').prop("checked", v);
-    $('.opt').prop("checked", v);
-    if(a=false){
-        console.log("ㅎㅇ");
-        $("#checkAll").prop("checked", false);
-    }else{
-        
-    }
-});
-
-
-
-$('.checks').click(function(){
-console.log($(this).prop('checked'))
-console.log($(this).prop('opt'))
-
-
-});
+// $('.checks').click(function(){
+//     console.log($(this).prop('checked'));
+// })
